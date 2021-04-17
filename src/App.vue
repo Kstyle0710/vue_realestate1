63c15350793d2f4e346dcb82db3dd93bb673ffc2
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-04-18 08:51:59 +0900

    data binding

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <h2>안녕하세요</h2>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+        <!-- html 속성 바인딩 구간 : 앞쪽에 콜론을 사용 -->
+    <h4 class='red' :style="style1">종로 원룸</h4>
+       <!-- 내용 데이터 바인딩 구간 : 쌍중괄호 -->
+    <p>{{price1}}</p>
+    <p>{{products[1]}}</p>  
+  </div>
+    <div>
+    <h4>강남 원룸</h4>
+    <p>{{price2}}</p>
+    <p>{{products[0]}}</p>  
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  // 데이터 보관함 만들기 (return 중괄호 안에 데이터를 담는다)
+  data(){
+    return {
+      price1 : 100,
+      price2 : 70,
+      style1 : 'color : blue',
+      products : ['역삼동 원룸', '천호동 원룸', '군산 원룸'],
+
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
